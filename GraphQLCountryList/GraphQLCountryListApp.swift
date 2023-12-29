fa7362391aa82bc3d51bf7c18a7e0a3bc2b49228
Mohit Tomer <mstomer31@gmail.com>
2023-12-29 15:07:18 +0530

    Initial Commit

---
+++ GraphQLCountryList/GraphQLCountryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLCountryListApp.swift
+//  GraphQLCountryList
+//
+//  Created by ꧁☆༒☬MoʜɩtToɱɘʀ☬༒☆꧂ on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLCountryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
